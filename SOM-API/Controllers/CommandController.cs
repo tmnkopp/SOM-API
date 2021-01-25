e23d64e0baa4378d973baad4d1519a8101c33398
Tim Kopp <tim.kopp@cyber-balance.com>
2021-01-25 10:24:07 -0500

    added parser supporting model

---
+++ SOM-API/Controllers/CommandController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using SOM.Parsers;
+using SOM.Procedures;
+using SOMData.Models;
+
+namespace SOM_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CommandController : ControllerBase
+    {
+        private readonly IConfiguration _config;
+        public CommandController(IConfiguration config)
+        {
+            _config = config; 
+        }
+        [HttpGet("GetAll")]
+        public IActionResult GetAll()
+        {
+            ConsoleCommand model = new ConsoleCommand();
+            model.ConsoleCommandID = 0;
+            model.CommandLine = " -t 2";
+            return new JsonResult(model);
+        }
+    } 
+}
